963f4e2c0a5680ebc76d146379a22156d9b887f1
abdallahibm <abdallahibmabdallah@gmail.com>
2025-12-15 16:50:08 +0200

    Update Admin Controller.Designer.cs

--- Admin Controller.Designer.cs
+++ Admin Controller.Designer.cs
@@ -53,30 +53,29 @@
             this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label1.Font = new System.Drawing.Font("Sitka Banner", 20.25F);
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label1.Location = new System.Drawing.Point(36, 85);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(48, 105);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 39);
+            this.label1.Size = new System.Drawing.Size(171, 49);
             this.label1.TabIndex = 0;
             this.label1.Text = "Username :";
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.textBox1.Location = new System.Drawing.Point(176, 102);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Location = new System.Drawing.Point(235, 126);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.Size = new System.Drawing.Size(169, 22);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Sitka Banner", 12F);
             this.button1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.button1.Location = new System.Drawing.Point(82, 148);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(109, 182);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 33);
+            this.button1.Size = new System.Drawing.Size(172, 41);
             this.button1.TabIndex = 2;
             this.button1.Text = "Delete Account";
             this.button1.UseVisualStyleBackColor = true;
@@ -85,10 +84,10 @@
             // 
             this.button2.Font = new System.Drawing.Font("Sitka Banner", 12F);
             this.button2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.button2.Location = new System.Drawing.Point(254, 148);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(339, 182);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(129, 33);
+            this.button2.Size = new System.Drawing.Size(172, 41);
             this.button2.TabIndex = 3;
             this.button2.Text = "Suspend Account";
             this.button2.UseVisualStyleBackColor = true;
@@ -97,10 +96,10 @@
             // 
             this.button3.Font = new System.Drawing.Font("Sitka Banner", 12F);
             this.button3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.button3.Location = new System.Drawing.Point(479, 63);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Location = new System.Drawing.Point(639, 78);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(185, 32);
+            this.button3.Size = new System.Drawing.Size(247, 39);
             this.button3.TabIndex = 4;
             this.button3.Text = "Create New Usher";
             this.button3.UseVisualStyleBackColor = true;
@@ -109,10 +108,10 @@
             // 
             this.button4.Font = new System.Drawing.Font("Sitka Banner", 12F);
             this.button4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.button4.Location = new System.Drawing.Point(479, 164);
-            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Location = new System.Drawing.Point(731, 193);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(185, 32);
+            this.button4.Size = new System.Drawing.Size(247, 39);
             this.button4.TabIndex = 5;
             this.button4.Text = "Create New Administrator";
             this.button4.UseVisualStyleBackColor = true;
@@ -121,10 +120,10 @@
             // 
             this.button5.Font = new System.Drawing.Font("Sitka Banner", 12F);
             this.button5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.button5.Location = new System.Drawing.Point(479, 114);
-            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Location = new System.Drawing.Point(639, 140);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(185, 32);
+            this.button5.Size = new System.Drawing.Size(247, 39);
             this.button5.TabIndex = 6;
             this.button5.Text = "Create New Agency";
             this.button5.UseVisualStyleBackColor = true;
@@ -135,10 +134,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Sitka Banner", 20.25F);
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label2.Location = new System.Drawing.Point(36, 292);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(48, 359);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(149, 39);
+            this.label2.Size = new System.Drawing.Size(188, 49);
             this.label2.TabIndex = 7;
             this.label2.Text = "Event Name:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -148,31 +146,30 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Sitka Banner", 20.25F);
             this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label3.Location = new System.Drawing.Point(412, 292);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(549, 359);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 39);
+            this.label3.Size = new System.Drawing.Size(64, 49);
             this.label3.TabIndex = 9;
             this.label3.Text = "By:";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(467, 309);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Location = new System.Drawing.Point(623, 380);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(197, 20);
+            this.textBox3.Size = new System.Drawing.Size(261, 22);
             this.textBox3.TabIndex = 11;
             // 
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Sitka Banner", 12F);
             this.button6.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.button6.Location = new System.Drawing.Point(407, 447);
-            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button6.Location = new System.Drawing.Point(543, 550);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(127, 32);
+            this.button6.Size = new System.Drawing.Size(169, 39);
             this.button6.TabIndex = 15;
             this.button6.Text = "Decline";
             this.button6.UseVisualStyleBackColor = true;
@@ -181,10 +178,10 @@
             // 
             this.button7.Font = new System.Drawing.Font("Sitka Banner", 12F);
             this.button7.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.button7.Location = new System.Drawing.Point(212, 447);
-            this.button7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button7.Location = new System.Drawing.Point(283, 550);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(127, 32);
+            this.button7.Size = new System.Drawing.Size(169, 39);
             this.button7.TabIndex = 14;
             this.button7.Text = "Approve";
             this.button7.UseVisualStyleBackColor = true;
@@ -195,10 +192,9 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Sitka Banner", 20.25F);
             this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label5.Location = new System.Drawing.Point(202, 354);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(269, 436);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(159, 39);
+            this.label5.Size = new System.Drawing.Size(201, 49);
             this.label5.TabIndex = 12;
             this.label5.Text = "Assign Usher:";
             this.label5.Click += new System.EventHandler(this.label5_Click);
@@ -206,19 +202,19 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(365, 371);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox1.Location = new System.Drawing.Point(487, 457);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(196, 21);
+            this.comboBox1.Size = new System.Drawing.Size(260, 24);
             this.comboBox1.TabIndex = 16;
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(189, 309);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox2.Location = new System.Drawing.Point(252, 380);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(194, 21);
+            this.comboBox2.Size = new System.Drawing.Size(257, 24);
             this.comboBox2.TabIndex = 17;
             // 
             // label4
@@ -227,10 +223,9 @@
             this.label4.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label4.Font = new System.Drawing.Font("Sitka Banner", 26.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label4.Location = new System.Drawing.Point(11, 9);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(15, 11);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(284, 50);
+            this.label4.Size = new System.Drawing.Size(356, 63);
             this.label4.TabIndex = 18;
             this.label4.Text = "Accounts Controller";
             // 
@@ -240,18 +235,17 @@
             this.label6.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label6.Font = new System.Drawing.Font("Sitka Banner", 26.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label6.Location = new System.Drawing.Point(11, 227);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(15, 279);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(253, 50);
+            this.label6.Size = new System.Drawing.Size(316, 63);
             this.label6.TabIndex = 19;
             this.label6.Text = "Events Controller";
             // 
             // Form3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(726, 534);
+            this.ClientSize = new System.Drawing.Size(1028, 657);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.comboBox2);
@@ -269,7 +263,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form3";
             this.Text = "Administrators Controller Form";
             this.Load += new System.EventHandler(this.Form3_Load);
