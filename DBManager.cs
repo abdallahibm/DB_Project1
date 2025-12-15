9190394a9b0bd06b0f98adce7c4342c3aa1330d3
abdallahibm <abdallahibmabdallah@gmail.com>
2025-12-15 15:00:17 +0200

    Update DBManager.cs

--- DBManager.cs
+++ DBManager.cs
@@ -22,7 +22,6 @@ namespace DBapplication
             {
                 myConnection.Open(); //Open a connection with the DB
 
-                // just for illustration when the database is opened, 
                 // this should NOT be shown in GUI to the user in the final application
                 // but we show it here only to make sure that the database is working
                 MessageBox.Show("Successfully connected to the database!");
